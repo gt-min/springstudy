08a2e6d8d3317c048ecedb788673403608f1df8f
gt-min <gt_min@naver.com>
2024-07-16 09:40:19 +0900

    20240716

--- boot/src/main/java/com/min/boot/utils/MailUtils.java
+++ boot/src/main/java/com/min/boot/utils/MailUtils.java
@@ -1,6 +1,8 @@
 package com.min.boot.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -8,11 +10,24 @@ import org.springframework.stereotype.Component;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
+/* application.properties 파일의 프로퍼티 값 읽어오기
+ * 
+ *   @org.springframework.context.annotation.PropertySource(value = "classpath:application.properties")
+ * 
+ *   @Autowired
+ *   private org.springframework.core.env.Environment env;
+ *   
+ *   env.getProperty("spring.mail.username")  */
+
+@PropertySource(value = "classpath:application.properties")
 @Component
 public class MailUtils {
   
   @Autowired
   private JavaMailSender javaMailSender;
+  
+  @Autowired
+  private Environment env;
   
   public void sendMail(String to, String subject, String content) {
     
@@ -20,7 +35,7 @@ public class MailUtils {
 
       MimeMessage mimeMessage = javaMailSender.createMimeMessage();
       MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-      helper.setFrom("gt_min@naver.com");
+      helper.setFrom(env.getProperty("spring.mail.username"));
       helper.setTo(new InternetAddress(to));
       helper.setSubject(subject);
       mimeMessage.setContent(content, "text/html; charset=UTF-8");
